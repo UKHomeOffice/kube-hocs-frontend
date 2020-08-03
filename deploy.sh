c8bb49697b4a8fe49473c7f8069115fc424b718f
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2020-08-03 15:59:34 +0100

    Consolidate if statements
    This commit tidies up the deploy.sh by grouping as many common
    configuration settings together based on the target cluster.

--- deploy.sh
+++ deploy.sh
@@ -16,34 +16,40 @@ export IP_WHITELIST=${POISE_WHITELIST}
 if [[ ${KUBE_NAMESPACE} == "cs-prod" ]] ; then
     echo "deploy ${VERSION} to PROD namespace, using HOCS_FRONTEND_PROD_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_PROD_CS}
-    export REPLICAS="2"
+    export DNS_PREFIX=www.cs
+    export NOTPROD=0
 elif [[ ${KUBE_NAMESPACE} == "wcs-prod" ]] ; then
     echo "deploy ${VERSION} to PROD namespace, using HOCS_FRONTEND_PROD_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_PROD_WCS}
-    export REPLICAS="2"
-elif [[ ${KUBE_NAMESPACE} == "cs-qa" ]] ; then
+    export NOTPROD=0
+    export DNS_PREFIX=www.wcs
+else
+  export NOTPROD="true"
+fi
+if [[ ${KUBE_NAMESPACE} == "cs-qa" ]] ; then
     echo "deploy ${VERSION} to QA namespace, using HOCS_FRONTEND_QA_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_QA_CS}
-    export REPLICAS="1"
+    export DNS_PREFIX=qa.internal.cs-notprod
 elif [[ ${KUBE_NAMESPACE} == "wcs-qa" ]] ; then
     echo "deploy ${VERSION} to QA namespace, using HOCS_FRONTEND_QA_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_QA_WCS}
-    export REPLICAS="1"
+    export DNS_PREFIX=qa.wcs-notprod
 elif [[ ${KUBE_NAMESPACE} == "cs-demo" ]] ; then
     echo "deploy ${VERSION} to DEMO namespace, HOCS_FRONTEND_DEMO_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEMO_CS}
-    export REPLICAS="1"
+    export DNS_PREFIX=demo.cs-notprod
 elif [[ ${KUBE_NAMESPACE} == "wcs-demo" ]] ; then
     echo "deploy ${VERSION} to DEMO namespace, HOCS_FRONTEND_DEMO_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEMO_WCS}
-    export REPLICAS="1"
+    export DNS_PREFIX=demo.wcs-notprod
 elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
     echo "deploy ${VERSION} to DEV namespace, HOCS_FRONTEND_DEV_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEV_CS}
-    export REPLICAS="1"
+    export DNS_PREFIX=dev.internal.cs-notprod
 elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
     echo "deploy ${VERSION} to DEV namespace, HOCS_FRONTEND_DEV_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEV_WCS}
+    export DNS_PREFIX=dev.wcs-notprod
     export REPLICAS="1"
 else
     echo "Unable to find environment: ${ENVIRONMENT}"
@@ -54,33 +60,12 @@ if [[ -z ${KUBE_TOKEN} ]] ; then
     exit -1
 fi
 
-if [[ "${KUBE_NAMESPACE}" == "wcs-prod" ]] ; then
-    export DNS_PREFIX=www.wcs
-    export KC_REALM=https://sso.digital.homeoffice.gov.uk/auth/realms/HOCS
-elif [[ "${KUBE_NAMESPACE}" == "cs-prod" ]] ; then
-    export DNS_PREFIX=www.cs
-    export KC_REALM=https://sso.digital.homeoffice.gov.uk/auth/realms/hocs-prod
-elif [[ "${KUBE_NAMESPACE}" == "cs-dev" ]] ; then
-    export DNS_PREFIX=dev.internal.cs-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
-elif [[ "${KUBE_NAMESPACE}" == "wcs-dev" ]] ; then
-    export DNS_PREFIX=dev.wcs-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
-elif [[ "${KUBE_NAMESPACE}" == "cs-qa" ]] ; then
-    export DNS_PREFIX=qa.internal.cs-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
-elif [[ "${KUBE_NAMESPACE}" == "wcs-qa" ]] ; then
-    export DNS_PREFIX=qa.wcs-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
-elif [[ "${KUBE_NAMESPACE}" == "cs-demo" ]] ; then
-    export DNS_PREFIX=demo.cs-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
-elif [[ "${KUBE_NAMESPACE}" == "wcs-demo" ]] ; then
-    export DNS_PREFIX=demo.wcs-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
+if [[ $NOTPROD == "true" ]]; then
+  export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk
+  export REPLICAS="1"
 else
-    export DNS_PREFIX=${ENVIRONMENT}.${DOMAIN}-notprod
-    export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
+  export KC_REALM=https://sso.digital.homeoffice.gov.uk/auth/realms/HOCS
+  export REPLICAS="2"
 fi
 
 export DNS_SUFFIX=.homeoffice.gov.uk
